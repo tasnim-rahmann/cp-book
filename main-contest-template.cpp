e6df2491aa944b7fb93ad5e2d52377d728eb325e
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-05-09 11:59:14 +0600

    10-05-2026

--- main-contest-template.cpp
+++ main-contest-template.cpp
@@ -51,12 +51,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t = 1;
+    int t;
     cin >> t;
 
     while(t--) {
         solve();
     }
+
+    // solve()
 
     return 0;
 }
